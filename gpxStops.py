20d7f08e106ccc5477ee1bfa2ea3a120ff0115bf
MarsFlyer <paul_t_allen@yahoo.com>
2026-02-11 22:21:45 +0000

    Refactored to enable pytests

--- gpxStops.py
+++ gpxStops.py
@@ -12,6 +12,13 @@ import sys
 from geopy.geocoders import Nominatim
 from ratelimit import limits, sleep_and_retry
 
+# Initialize Nominatim API 
+geolocator = Nominatim(user_agent="my_geopy_app")
+# Test a location
+# location = getLocation(52.108352,0.924454)
+# print(location)
+# exit()
+
 # Settings
 secondsGap = 120 # seconds
 speedLow = 0.5 # metres per second. 1 m/s = 3.6 km/s
@@ -21,18 +28,11 @@ unitsDivider = 1609 # metres per mile
 units = 'miles'
 url = 'https://www.google.com/maps/place/'
 
-# Get GPX file name and option from arguments
-file = sys.argv[1]
-getLocations = True
-if  len(sys.argv) > 2:
-    if sys.argv[2].lower() in ("false", "no", "0"):
-        getLocations = False
-
 # This throttles rqeuests to this the free service. Max of once every 2 seconds.
 @sleep_and_retry
 @limits(calls=1, period=2)
-def getLocation(lat, lon):
-    if getLocations == False:
+def getLocation(lat, lon, getLocations=True):
+    if  getLocations == False:
         return ''
     try:
         # print('Reverse lookup' + str(lat) + ',' + str(lon))
@@ -96,66 +96,74 @@ def getLocation(lat, lon):
     name = ", ".join(parts)
     return name
 
-# Initialize Nominatim API 
-geolocator = Nominatim(user_agent="my_geopy_app")
-# Test a location
-# location = getLocation(52.108352,0.924454)
-# print(location)
-# exit()
+def process_gpx_file(file_path, getLocations=True, settings=None):
 
-# Get GPX file and parse it
-gpx_file = open(file, 'r')
-gpx = gpxpy.parse(gpx_file)
-point_data = gpx.get_points_data()
+    # Get GPX file and parse it
+    gpx_file = open(file_path, 'r')
+    gpx = gpxpy.parse(gpx_file)
+    point_data = gpx.get_points_data()
 
-i = -1
-stopped = None
-location = ''
-speed = 0
-for track in gpx.tracks:
-    for segment in track.segments:
-        timeLast = None
-        for point in segment.points:
-            i += 1
-            if i==0:
-                timeFirst = point.time
-                print('Start: {0}'.format(point.time.astimezone()))
-            timeThis = point.time
-            if timeLast == None:
+    i = -1
+    stopped = None
+    location = ''
+    speed = 0
+    output = ''
+    for track in gpx.tracks:
+        for segment in track.segments:
+            timeLast = None
+            for point in segment.points:
+                i += 1
+                if i==0:
+                    timeFirst = point.time
+                    print('Start: {0}'.format(point.time.astimezone()))
+                timeThis = point.time
+                if timeLast == None:
+                    pointLast = point
+                    timeLast = point.time
+                    continue
+                interval = timeThis - timeLast
+                speed = point.speed_between(pointLast)
+                # print('Interval: ',interval)
+                if interval.seconds > secondsGap and speed > speedLow:
+                    if stopped == None:
+                        stopped = interval
+                    else:
+                        stopped += interval
+                    distance = point_data[i].distance_from_start
+                    if getLocations:
+                        location = getLocation(pointLast.latitude, pointLast.longitude)
+                    if interval.seconds > 60:
+                        stop = '{0} minutes'.format(int(interval.seconds/60))
+                    else:
+                        stop = '{0} seconds'.format(int(interval.seconds))
+                    # Example: YYYY-MM-DD HH:MM:SS+HH:MM n minutes at 1.2 miles. Location: Village, Town, County https://www.google.com/maps/place/51.123456,-0.123456
+                    print('{0} {1} at {4:0.1f} {5}. Location: {6} {7}{2},{3}'.format(timeLast.astimezone(), stop, pointLast.latitude, pointLast.longitude, distance/unitsDivider, units, location, url))
+                    distanceLast = point_data[i-1].distance_from_start
+                    if distance - distanceLast > distanceGap:
+                        location = '^'
+                        if getLocations and (distance - distanceLast > locationGap):
+                            location = getLocation(point.latitude, point.longitude)
+                        print('Distance missed: {0:0.1f} {1} at lat,lon: {2},{3} location: {4}'.format((distance-distanceLast)/unitsDivider, units, point.latitude, point.longitude, location))
+                # print('{0} speed {1}'.format(i, speed))
                 pointLast = point
-                timeLast = point.time
-                continue
-            interval = timeThis - timeLast
-            speed = point.speed_between(pointLast)
-            # print('Interval: ',interval)
-            if interval.seconds > secondsGap and speed > speedLow:
-                if stopped == None:
-                    stopped = interval
-                else:
-                    stopped += interval
-                distance = point_data[i].distance_from_start
-                if getLocations:
-                    location = getLocation(pointLast.latitude, pointLast.longitude)
-                if interval.seconds > 60:
-                    stop = '{0} minutes'.format(int(interval.seconds/60))
-                else:
-                    stop = '{0} seconds'.format(int(interval.seconds))
-                # Example: YYYY-MM-DD HH:MM:SS+HH:MM n minutes at 1.2 miles. Location: Village, Town, County https://www.google.com/maps/place/51.123456,-0.123456
-                print('{0} {1} at {4:0.1f} {5}. Location: {6} {7}{2},{3}'.format(timeLast.astimezone(), stop, pointLast.latitude, pointLast.longitude, distance/unitsDivider, units, location, url))
-                distanceLast = point_data[i-1].distance_from_start
-                if distance - distanceLast > distanceGap:
-                    location = '^'
-                    if getLocations and (distance - distanceLast > locationGap):
-                        location = getLocation(point.latitude, point.longitude)
-                    print('Distance missed: {0:0.1f} {1} at lat,lon: {2},{3} location: {4}'.format((distance-distanceLast)/unitsDivider, units, point.latitude, point.longitude, location))
-            # print('{0} speed {1}'.format(i, speed))
-            pointLast = point
-            if speed > speedLow:
-                timeLast = point.time
-            # else:
-            #     print('{0} time {2} speed {1} last {3}'.format(i, speed, point.time, timeLast))
-            # print('Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation))
-            # if i > 10:
-            #     break
-    interval = (timeLast - timeFirst)
-    print('End: {0} duration: {1} stopped: {2}'.format(timeLast.astimezone(), interval, stopped))
+                if speed > speedLow:
+                    timeLast = point.time
+                # else:
+                #     print('{0} time {2} speed {1} last {3}'.format(i, speed, point.time, timeLast))
+                # print('Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation))
+                # if i > 10:
+                #     break
+        interval = (timeLast - timeFirst)
+        print('End: {0} duration: {1} stopped: {2}'.format(timeLast.astimezone(), interval, stopped))
+
+def main():
+    # Get GPX file name and option from arguments
+    file = sys.argv[1]
+    getLocations = True
+    if  len(sys.argv) > 2:
+        if sys.argv[2].lower() in ("false", "no", "0"):
+            getLocations = False
+    process_gpx_file(file, getLocations)
+
+if __name__ == '__main__':
+    main()
